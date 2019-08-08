1f13f00ca639a9cfbe46e4861e07001db0192333
github username <github email address>
2019-08-08 14:09:04 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
-import SimpleComponent from './components/SimpleComponent';
-import SimplerComponent from './components/SimplerComponent';
+import SimpleComponent from "./components/SimpleComponent";
+import SimplerComponent from "./components/SimplerComponent";
+
+const logging = () => {
+  console.log("Hi!");
+};
 
 ReactDOM.render(
   <div>
     <SimpleComponent />
-    <SimplerComponent handleClick={undefined} />
+    <SimplerComponent handleClick={logging} />
   </div>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
